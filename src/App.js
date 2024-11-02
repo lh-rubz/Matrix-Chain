ff413876508e963c8b80207ef30fd79a0eb64355
lh-rubz <habuelrub@gmail.com>
2024-11-02 15:31:22 +0200

    edited packages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import MatrixChainVisualizer from "./MatrixChainVisualizer";
 import "./App.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 function App() {
   return (
     <div className="App">
